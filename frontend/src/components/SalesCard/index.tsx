a0fba4b4718ea1fcbacea076f3112770452517e2
Andrei Robson da Silva <andreirobson@furb.br>
2022-07-16 18:30:30 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,9 +20,9 @@ function SalesCard() {
   const [sale, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
+
     const dmin = minDate.toISOString().slice(0, 10);
     const dmax = maxDate.toISOString().slice(0, 10);
-
 
     axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
       .then(response => {
@@ -77,7 +77,7 @@ function SalesCard() {
                   <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
